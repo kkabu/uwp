c234e15a822bf7549eb9b768f50b24363f2d9b7c
yuchnegye <yuchengye2013@outlook.com>
2016-08-24 02:57:29 +0800

    new

---
+++ icheck1/icheck1/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace icheck1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            WebRequest.Create("http://ifxw.net/icheck/cr.php??Name=yuchengye&Tel=2333&TouchID=80&Grade=100");
+
+        }
+        private void Upd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
